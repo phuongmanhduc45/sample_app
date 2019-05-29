b22f2095c4c7e4ece957d843edab966ea41ccce3
phuong manh duc <phuongmanhduc123@gmail.com>
2019-05-29 10:18:42 +0700

    Finish user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,16 @@ class User < ApplicationRecord
   before_save {email.downcase!}
   has_secure_password
 
-  validates :name, presence: true,
-    length: Settings.name_length_maximum
+  validates :name,
+    presence: true,
+    length: {maximum: Settings.name_length_maximum}
+
   validates :email,
-    presence: true, length: Settings.email_length_maximum,
+    presence: true,
+    length: {maximum: Settings.email_length_maximum},
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
-  validates :password, presence: true,
-    length: Settings.password_length_minimum
 
+  validates :password, presence: true,
+    length: {minimum: Settings.password_length_minimum}
 end
